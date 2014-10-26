4c3a1756ae53204c9412c00b9f2ab4d3152c31e0
adamcee <adam.cahan@gmail.com>
2014-10-25 20:00:01 -0700

    animation handling/swatchspace redrawing working in displayColor...js.

--- public/javascripts/displayColorOfTheDay.js
+++ public/javascripts/displayColorOfTheDay.js
@@ -16,69 +16,109 @@
 
 /********************************Main Application*************************/
 
-//Properties of the coordinate space we are displaying our swatches in
+/**Creating variables and such**
+ * ****************************/
+
 /*Set Canvas width and height - eventually these should be derived from CSS container of canvas*/
 view.viewSize = [600,600];
 
-//Constructor func. Simple. 
+/*Instantiate minSpace SwatchSpace for minute's worth of watches. Associated vars.*/
 var newMinSwatch = function(){
   //Necessary to clone before maths op b/c otherwise view.center appears to be modified by Paper.js maths op
   return new SwatchSpace(500, 200, 5,3,view.center.clone() -{x:0,y:150} );
 }
-var minSwatch = newMinSwatch();
 
-var cpArr = minSwatch.centerPoints;
-var activeSwatch = minSwatch.activeSwatch;//easier typing
+var minSpace = newMinSwatch();
+var cpArr = minSpace.centerPoints;
+var activeSwatch = minSpace.activeSwatch;//easier typing
 
-//hack - draw circle @ center of screen for reference
+/*Hold Paper.js groups ('swatches') which are 'in transit' - being animated*/
+var swatchesInTransit = [];
+
+/*Hacks to draw visual reference points for animation. Remove for production*/
+//center of canvas
 centerCirc = new Path.Circle(view.center, 20);
 centerCirc.strokeColor = 'black';
 centerCirc.fillColor = 'pink'
 
-
-//hack - draw box around paper.js space (which should be canvas size)
+//outline canvas
 boundRect = new Path.Rectangle([0,0], view.viewSize);
 boundRect.strokeColor = 'black';
 
-
-
-//hack - show where swatch space should go
-var destination = new Point(minSwatch.ssCenterPoint + [0,330]);//NOTE 10/24/14: Is this modifying ssCenterPoint ????
+//destination of minSpace
+var destination = new Point(minSpace.ssCenterPoint + [0,330]);//NOTE 10/24/14: Is this modifying ssCenterPoint ????
 mark = new Path.Circle(destination, 10);
 mark.fillColor = 'pink';
 mark.strokeColor = 'black';
-/*** Websocket Stuff And Animation/drawing Swatches  ***/
+
+
+/**Paper.js Animation -- Mainly for animating SwatchSpaces**
+************************************************************/
+function onFrame(event){
+ 
+  if(minSpace){
+    //Iterate thru an array of all 'minutes' currently in transit and animate.
+    for(var i = 0; i < swatchesInTransit.length; i++){
+      curSwatch = swatchesInTransit[i];
+      var vector = destination - curSwatch.position;
+      curSwatch.position += vector / 40;
+
+      if(curSwatch.position == destination){
+        //add curSwatch to hourSpace SwatchSpace and pop from swatchesInTransit[]
+      }
+    }
+    /*
+    if(minSpace.isSpaceFull()){ //We will drop this
+    }
+    */
+  }
+}
+
+
+
+
+/**Sockets. Triggering actions based on socket messages**
+ * *****************************************************/
 
 //CHANGE TO YOUR SETTINGS. Currently set for Heroku deployment
 var socket = io.connect(window.location.hostname);
 
-//confirm connection
 socket.on('test', function(data){
   console.log("websocket test message: ", data);
 });
 
-/**Display and scroll tweets**/
+//Display and scroll tweets
 //LINE OF JQUERY TO INSERT UL ON ID 'tweet' GOES HERE
 
-//when new colorstamp received draw swatch to represent it
+
+//when new colorstamp received draw swatch to represent it.
 socket.on('colorstamp', function(colorstamp){
   
-  //Draw a swatch
   var swatchColor = colorstamp.modeColors[0];//for now ignore ties...
-  //minSwatch.drawSwatchesForever(swatchColor, createSmallSwatch);
-  minSwatch.drawNextSwatch(swatchColor, createSmallSwatch);
+
+  //When minSpace full add group to arr for animation and reset minSpace.
+  if(!minSpace.isSpaceFull()){
+    console.log('Drawing a '+swatchColor + ' swatch...');
+    minSpace.drawNextSwatch(swatchColor, createSmallSwatch);
+  }
+  else{
+    swatchesInTransit.push(minSpace.group.clone());
+    minSpace.deleteSwatches();
+    view.update();
+  }
+
   view.update();//needed to re-render canvas correctly on draw
 
   /*** test/debug ***/
   //iterate thru colorswatch obj. a lot of console messages. Note client receives LOTS more data than we currently use...
   console.log('Received colorstamp! start: '+colorstamp.start+ ' end: '+colorstamp.end);
   colorstamp.modeColors.forEach(function(color){console.log("Top color is: ",color)});
-  console.log('minSwatch.Swatchfull is: ' + minSwatch.swatchFull);
+  console.log('minSpace.Swatchfull is: ' + minSpace.isSpaceFull());
 
   var allColors = colorstamp.allColors;
   var colorKeys = Object.keys(allColors);
 
-  /*
+  /*Degbug
   console.log('Count of top color is: ',colorstamp.modeCount);
   console.log('Additional colors.....');
   for(var i = 0; i< colorKeys.length; i++){
@@ -89,17 +129,6 @@ socket.on('colorstamp', function(colorstamp){
 });//close socket.on('colorstamp'
 
 
-/**Paper.js Animation -- Mainly for animating SwatchSpaces**
-************************************************************/
-function onFrame(event){
-  
-  if(minSwatch){
-    if(minSwatch.swatchFull){
-      var vector = destination - minSwatch.group.position;
-      minSwatch.group.position += vector / 40;
-    }
-  }
-}
 
 /*************************************************************************************************************************
  * Function: SwatchSpace
@@ -117,32 +146,36 @@ function onFrame(event){
  ************************************************************************************************************************/
 function SwatchSpace(pixelWidth, pixelHeight, xSpace, ySpace, ssCenterPoint){
 
-  var ss = {
-    //vars
+  var ss = { /*SwatchSpace Object*/
+
+    /****Variables*****/
     group: new Group(),//Paper.js group for manipulating entire SwatchSpace easily
-    width: pixelWidth,
-    height: pixelHeight,
-    xSpace: xSpace,
-    ySpace: ySpace,
-    ssCenterPoint: null, 
+    width: pixelWidth, height: pixelHeight, xSpace: xSpace, ySpace: ySpace, maxSwatches: xSpace*ySpace, ssCenterPoint: null, 
 
-    totalSwatches: 0,//set w/setSwatchSize init func
-    swatchSize: null,//Will be replaced w/paper.js Size obj on init
-    firstPoint: null,//will be replaced w/paper.js Point obj on init
-    swatchFull: false,//true if all gridpoints in swatch are 'filled', i.e. rendered
+    //Will be replaced w/paper.js Size obj on init
+    swatchSize: null, firstPoint: null,
 
-    swatches: [], 
-    activeSwatch_pointer: 0, //pointer for swatches[]. Wraps around to limit max # of swatches. 
-    centerPoints:  [],
-    cp_pointer: 0,//pointer for centerPoints arr to track most recently created swatch
-    activeSwatch: null, //hold Paper obj of most recently created or 'active' swatch. cp_pointer points to centerpoint for this swatch. Prob should consolidate data structures
+    swatches: [], centerPoints:  [],
+
+    //pointers track most recently crated swatches. Implementation wraps them around to reset to 0 after they hit maxSwatches
+    activeSwatch_pointer: 0, cp_pointer: 0,
+    activeSwatch: null, //hold Paper obj of most recently created or 'active' swatch.     
+
+    /****Functions*****/
     
-    //funcs
-    setSwatchSize: function(){ //Also sets totalSwatches val
+    //Clear the SwatchSpace of all its swatches. Centerpoints, etc, maintained.
+    deleteSwatches: function(){
+      this.swatches = [];
+      this.activeSwatch = null;
+      this.activeSwatch_pointer = 0, this.cp_pointer = 0;
+      this.group.remove(), this.group = new Group();//Needed for Paper.js to remove old rendering and then to render new swatches
+      console.log('Ran deleteSwatches: swatches[] len is ' + this.swatches.length + ' , acSwatch and cp pointers are ' + this.activeSwatch_pointer + ' and ' + this.cp_pointer);
+    },
+
+    setSwatchSize: function(){ //Also sets maxSwatches val
                      this.swatchSize = new Size(this.width/this.xSpace, this.height/this.ySpace);
     },
 
-    
     //Must be run after setSwatchSize. SwatchSpace uses 'top left' coordinate as start point for drawing swatches
     setTopLeftPoint: function(ssCenterPoint){
                       this.ssCenterPoint = ssCenterPoint;
@@ -151,10 +184,9 @@ function SwatchSpace(pixelWidth, pixelHeight, xSpace, ySpace, ssCenterPoint){
                                             );
                      },
 
-    setTotalSwatches: function(){
-                     this.totalSwatches = this.xSpace*this.ySpace;
+    setMaxSwatches: function(){
+                     this.maxSwatches = this.xSpace*this.ySpace;
                       },
-
 
     testSwatchSize: function(){//test func
                       console.log('testing swatch size...');
@@ -184,49 +216,46 @@ function SwatchSpace(pixelWidth, pixelHeight, xSpace, ySpace, ssCenterPoint){
     init: function(){
             this.setSwatchSize();//Must run before setTopLeftPoint()
             this.setTopLeftPoint(ssCenterPoint);
-            this.setTotalSwatches();
             this.generateCenterPoints();
           },
 
-    //Applies a swatch-rendering function of choice to draw Swatch at a given point
-    /* Func: drawSwatch
-     * Args: Point, Size, Color, Function
-     * Notes: renderFunc arg must return a Paper.js Object (should be a Swatch). renderFunc does the actual rendering on screen.
-     */
-    drawSwatch:  function(centerPoint, size, color, renderFunc){
-       renderedSwatch = renderFunc(centerPoint, size, color);
-       this.swatches[this.activeSwatch_pointer] = renderedSwatch;
-       this.activeSwatch_pointer += 1 % (this.totalSwatches);//total # of swatches. Array 'wraps' back around, ring-buffer-like.
-       return renderedSwatch
-       
-    },
+    //Adds a swatch to the SwatchSpace. Updates pointers. Returns the swatch unchanged.
+    addSwatch: function(swatch /*Paper.js Group or Item*/){
+       this.swatches[this.activeSwatch_pointer] = swatch;
+       this.moveAcSwatchPointer();
+       this.cp_pointer +=1;//awkward. Maybe subsume in moveAcSwatchPointer()? Something...
+       this.group.addChild(swatch);//Add swatch to Paper.js group
+       return swatch; 
+     },
 
     //Move activeSwatch_pointer to next position. Essentially using swatches[] like a ring buffer
     moveAcSwatchPointer: function(){
-       this.activeSwatch_pointer += 1 % (this.xSpace*this.ySpace);//total # of swatches. Array 'wraps' back around, ring-buffer-like.
+       this.activeSwatch_pointer += 1 % (this.maxSwatches);//Array 'wraps' back around, ring-buffer-like.
     },
-                         
-                             
+
+    isSpaceFull: function(){
+       return this.swatches.length == this.maxSwatches ? true : false;
+     },
+
+    //Applies a swatch-rendering function of choice to draw Swatch at a given point
+    //NOTE: renderFunc arg must return a Paper.js Object (should be a Swatch). renderFunc does the actual rendering on screen.
+    drawSwatch:  function(centerPoint, size, color, renderFunc){
+      console.log('in drawSwatch() -- centerPoint: ' + centerPoint); 
+       return this.addSwatch(renderFunc(centerPoint, size, color)); 
+    },
 
     //Draw swatch @ centerpoint[cp_pointer] - used to fill swatchspaces in an order..
     //Checks pointer position and will not draw once array size is reached
-    //Returns true on successful draw and false on max reached
     drawNextSwatch:  function(color, renderFunc){
-      if(this.cp_pointer < this.centerPoints.length){
+      if(!this.isSpaceFull()){ 
         //Render swatch and add to Group
         var renderedSwatch = this.drawSwatch(this.centerPoints[this.cp_pointer],this.swatchSize,color,renderFunc);
-        this.group.addChild(renderedSwatch);
-        this.cp_pointer +=1;
         console.log('drawing gridPos ',this.cp_pointer);
-        return true;
       }
       else{
-        this.swatchFull = true;
         console.log('Swatch is Full according to drawNextSwatch');
-        return false;
       }
     },
-
 
     //Like drawNextSwatch but 'loops' - draws over from initial pos once swatchspace is full
     //Return statement is an anachronism BUT allows this to be interchanged w/drawNextSwatch easily
@@ -324,6 +353,7 @@ function createSmallSwatch(centerPoint, theSize, color){
     mySwatch.position = centerPoint;
     mySwatch.fillColor = color;
 
+    console.log ('...' + color + ' swatch rendered');
     //Return the Group so it can be used
     return mySwatch;
 }
